4184b99fe8daf750727925347e5eaee1ccdd8b05
Nguyen Canh Quang <62925304+CanhQuangg@users.noreply.github.com>
2020-05-08 21:08:46 +0700

    Update CacTruongHopGiaoDich.java

--- NopBaiChinhThuc/src/truonghop/CacTruongHopGiaoDich.java
+++ NopBaiChinhThuc/src/truonghop/CacTruongHopGiaoDich.java
@@ -1,15 +1,13 @@
 package truonghop;
 
-public class CacTruongHopGiaoDich {
-		public static void truongHop1(int soDuTaiKhoan) {
+public cpublic static void truongHop1(int soDuTaiKhoan) {
 			
 			System.out.println("\n+----------------------+");
 			System.out.println("|   THEO YEU CAU HAI   |");
 			System.out.println("+----------------------+\n");
 			
-			System.out.println("\t======== GIA TRAN ========\n");
-			
 			System.out.println("\t >> Cac truong hop co the xay ra:");
+			System.out.println("\t =================================");
 			
 			System.out.println("\n===============");
 			System.out.println("|  A - B - C  |");
@@ -19,21 +17,35 @@ public class CacTruongHopGiaoDich {
 			int tranB = bangchungkhoan.GiaChungKhoan.tranB();
 			int tranC = bangchungkhoan.GiaChungKhoan.tranC();
 			
-			int tienDu = soDuTaiKhoan;
+			int sanA = bangchungkhoan.GiaChungKhoan.sanA();
+			int sanB = bangchungkhoan.GiaChungKhoan.sanA();
+			int sanC = bangchungkhoan.GiaChungKhoan.sanA();
 			
-			int muaTranA = (tienDu / 2) / tranA;
-			tienDu = tienDu - muaTranA*tranA;
-			int muaTranB = (tienDu/2) / tranB;
-			tienDu = tienDu - muaTranB*tranB;
-			int muaTranC = (tienDu/2) / tranC;
 			
-			System.out.println("=========================================");
-			System.out.println(" MA CO PHIEU          SO LUONG MUA DUOC");
-			System.out.println("-----------------------------------------");
-			System.out.println("     A                       " + muaTranA);
-			System.out.println("     B                       " + muaTranB);
-			System.out.println("     C                       " + muaTranC);
-			System.out.println("=========================================\n");
+			int tienDu1 = soDuTaiKhoan;
+			
+			int muaTranA = (tienDu1 / 2) / tranA;
+			tienDu1 = tienDu1 - muaTranA*tranA;
+			int muaTranB = (tienDu1/2) / tranB;
+			tienDu1 = tienDu1 - muaTranB*tranB;
+			int muaTranC = (tienDu1/2) / tranC;
+			
+			int tienDu2 = soDuTaiKhoan;
+			
+			int muaSanA = (tienDu2 / 2) / sanA;
+			tienDu2 = tienDu2 - muaSanA*sanA;
+			int muaSanB = (tienDu2 / 2) / sanB;
+			tienDu2 = tienDu2 - muaSanB*sanB;
+			int muaSanC = (tienDu2 / 2) / sanC;
+			tienDu2 = tienDu2 - muaSanC*sanC;
+			
+			System.out.println("=========================================================================");
+			System.out.println(" MA CO PHIEU          SL MUA DUOC GIA TRAN          SL MUA DUOC GIA SAN");
+			System.out.println("-------------------------------------------------------------------------");
+			System.out.println("     A                       " + muaTranA + "                               " + muaSanA);
+			System.out.println("     B                       " + muaTranB + "                               " + muaSanB);
+			System.out.println("     C                       " + muaTranC + "                               " + muaSanC);
+			System.out.println("=========================================================================\n");
 		}
 
 		public static void truongHop2(int soDuTaiKhoan) {	
